2a9d5fd9533491510a3706afebdb744d946b8981
Rodolfo Castillo Vidrio <rodolfo.castillo@globant.com>
2017-05-08 17:31:34 -0500

    Started Unit Testing?

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 var http = require('http');
 var weatherController = require('./services/weather.js');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello New York\n');
-}).listen(3001);
+
 weatherController.getWeatherCall(30,30);
-console.log('Server running at http://localhost:3001/');
--- services/weather.js
+++ services/weather.js
@@ -10,8 +10,8 @@ var weatherChocolate = "8ece0e93082d34c157fb30b8d73c5e60";
 
 exports.getWeatherCall = function(lat, lon) {
   console.log("SHOULD RECIEVE WEATHER SOON");
-  var url = "api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&APPID="+weatherChocolate;
-  var req = http.get(url, function(result){
+  var url = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&APPID="+weatherChocolate;
+  var req = https.get(url, function(result){
     console.log(consoleDebuggingString + "getting weather:::STATUS:" + result.statusCode);
     console.log(consoleDebuggingString + "getting weather:::HEADERS:" + JSON.stringify(result.headers));
 

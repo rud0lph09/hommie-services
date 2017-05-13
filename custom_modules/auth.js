432932c692912549fb9a3f40050767a3d6552b43
Rodolfo Castillo <raindeerdeveloper@gmail.com>
2017-05-13 17:38:30 -0500

    Weather Test Service EXTENSIVE

--- custom_modules/auth.js
+++ custom_modules/auth.js
@@ -3,6 +3,8 @@
 exports.simpleApiAuthWasSuccessful = function(apiSecret){
   if (apiSecret == process.env.API_ACCESS_TOKEN) {
     return true;
+  } else if (apiSecret == null){
+    return false;
   } else {
     return false;
   }
